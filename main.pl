76aad59464d13428aa8e48c6dffb2354242744f5
HamzaElKa <marokispots2911@gmail.com>
2025-12-18 10:27:07 +0100

    modif main

--- main.pl
+++ main.pl
@@ -1,27 +1,16 @@
+% =========================
 % main.pl
-% Point d'entrée du programme :
-% - On choisit le type de joueur pour 'x' (Joueur 1) et pour 'o' (Joueur 2)
-% - Puis on lance la partie avec ces deux "contrôleurs" (humain ou IA).
+% =========================
 
 :- [game].
 :- [ia_v1].
 :- [ia_v2].
 :- [ia_minimax].
-
-% Nouvelles IA
 :- [ia_alphabeta].
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %   Saisie des joueurs    %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% choix type :
-%   human               -> humain
-%   ia_v1               -> IA aléatoire
-%   ia_v2               -> IA "winning move"
-%   ia_minimax          -> IA minimax
-%   ia_alphabeta        -> IA alpha-beta
-%   ia_alphabeta_avance -> IA iterative deepening / alpha-beta avancee
 
 choose_player(PlayerSymbol, Type) :-
     format("Configure ~w :~n", [PlayerSymbol]),
@@ -48,7 +37,6 @@ map_choice_to_type(5, ia_alphabeta).
 %     Coup des joueurs     %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Demander un coup à un humain
 ask_move_human(Player, Board, Col) :-
     format("Player ~w (human), choose a column (1-7): ", [Player]),
     read(Input),
@@ -56,36 +44,91 @@ ask_move_human(Player, Board, Col) :-
       between(1, 7, Input),
       valid_col(Board, Input)
     ->
-        Col = Input
+      Col = Input
     ;
-        writeln('Invalid column, please try again.'),
-        ask_move_human(Player, Board, Col)
+      writeln('Invalid column, please try again.'),
+      ask_move_human(Player, Board, Col)
     ).
 
-% Récupérer le type du joueur courant ('x' ou 'o')
 get_player_type('x', TypeP1, _TypeP2, TypeP1).
 get_player_type('o', _TypeP1, TypeP2, TypeP2).
 
-% Helper IA universel
-call_ai(Type, Board, Player, Col) :-
-    Goal =.. [Type, Board, Player, Col],
-    call(Goal).
+% ---------- Appel IA (gère les signatures différentes)
+call_ai(ia_v1, Board, Player, Col) :-
+    ia_v1(Board, Player, Col).
 
+call_ai(ia_v2, Board, Player, Col) :-
+    ia_v2(Board, Player, Col).
+
+call_ai(ia_alphabeta, Board, Player, Col) :-
+    ia_alphabeta(Board, Player, Col).
+
+% minimax est codé "vu de o" => adaptation si Player = x
+call_ai(ia_minimax, Board, 'o', Col) :-
+    ia_minimax(Board, Col).
+
+call_ai(ia_minimax, Board, 'x', Col) :-
+    swap_board(Board, Swapped),
+    ia_minimax(Swapped, Col).
+
+% swap_board : échange x <-> o pour réutiliser minimax (qui maximise o)
+swap_board(Board, Swapped) :- maplist(swap_row, Board, Swapped).
+swap_row([], []).
+swap_row([H|T], [H2|T2]) :- swap_cell(H, H2), swap_row(T, T2).
+swap_cell('x','o') :- !.
+swap_cell('o','x') :- !.
+swap_cell('.','.').
+
+% ---------- Get move selon type
 get_move(human, Player, Board, Col) :-
     ask_move_human(Player, Board, Col).
 
-get_move(ia_v1, Player, Board, Col) :-
-    ia_v1(Board, Player, Col),
-    format("IA v1 (~w) plays in column ~w~n", [Player, Col]).
+get_move(Type, Player, Board, Col) :-
+    Type \= human,
+    call_ai(Type, Board, Player, Col),
+    format("~w (~w) plays in column ~w~n", [Type, Player, Col]).
 
-get_move(ia_v2, Player, Board, Col) :-
-    ia_v2(Board, Player, Col),
-    format("IA v2 (~w) plays in column ~w~n", [Player, Col]).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%      Boucle de jeu      %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-get_move(ia_minimax, Player, Board, Col) :-
-    ia_minimax(Board, Col),
-    format("IA minimax (~w) plays in column ~w~n", [Player, Col]).
+play_loop(Player, TypeP1, TypeP2) :-
+    board(Board),
+    displayBoard,
+    get_player_type(Player, TypeP1, TypeP2, Type),
+    format("New turn for: ~w (~w)~n", [Player, Type]),
+    get_move(Type, Player, Board, Col),
 
-get_move(ia_alphabeta, Player, Board, Col) :-
-    call_ai(ia_alphabeta, Board, Player, Col),
-    format("IA alpha-beta (~w) plays in column ~w~n", [Player, Col]).
+    ( playMove(Board, Col, NewBoard, Player) ->
+        applyIt(Board, NewBoard),
+
+        ( gameover(NewBoard) ->
+            displayBoard,
+            format("Player ~w wins!~n", [Player]),
+            retractall(board(_))
+        ; board_full(NewBoard) ->
+            displayBoard,
+            writeln('Draw!'),
+            retractall(board(_))
+        ;
+            changePlayer(Player, NextPlayer),
+            play_loop(NextPlayer, TypeP1, TypeP2)
+        )
+    ;
+        writeln('Move failed, retrying.'),
+        play_loop(Player, TypeP1, TypeP2)
+    ).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%          MAIN           %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+main :-
+    writeln('======================'),
+    writeln('       PUISSANCE 4    '),
+    writeln('======================'),
+    writeln('Configure players:'),
+    choose_player('Player 1 = x', TypeP1),
+    choose_player('Player 2 = o', TypeP2),
+    init_board,
+    play_loop('x', TypeP1, TypeP2).
